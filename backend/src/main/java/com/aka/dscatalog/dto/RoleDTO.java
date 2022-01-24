d8951d17c5a14e296d71e1dddb2324dfbdf60d0d
Geraldo-git <laboratorio.inicial@gmail.com>
2022-01-24 09:00:13 -0300

    Inseridas tabelas User e Role

---
+++ backend/src/main/java/com/aka/dscatalog/dto/RoleDTO.java
@@ -0,0 +1,10 @@
+package com.aka.dscatalog.dto;
+
+import java.io.Serializable;
+
+public class RoleDTO implements Serializable {
+    private Long id;
+    private String authority;
+
+    public RoleDTO(){}
+}
---
+++ backend/src/main/java/com/aka/dscatalog/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.aka.dscatalog.dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
